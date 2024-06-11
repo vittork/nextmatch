379dff71a0285d5fe0c9e58905c711a5805e28e5
Vittorio Rastama <vittorio.rastama@gasnet.id>
2024-06-12 03:37:09 +0700

    End of Section 2

---
+++ app/members/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function MembersPage() {
+  return (
+    <div>
+      <h3 className="text-3xl">This Will be the members page</h3>
+      <Link href="/">Go Back Home</Link>
+    </div>
+  );
+}
